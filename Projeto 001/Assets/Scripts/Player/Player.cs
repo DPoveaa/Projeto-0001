4365efeec0855dcdba972fad920ef6db341e0386
Povea <danieldpcaires@gmail.com>
2023-02-12 21:52:20 -0300

    .
    tristeza

--- Projeto 001/Assets/Scripts/Player/Player.cs
+++ Projeto 001/Assets/Scripts/Player/Player.cs
@@ -6,29 +6,60 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    private float horizontal;
-    public float speed;
-    public float jumpingPower;
-    private bool isFacingRight = true;
-    public int maxJumps;
-    private int jumps;
+    #region Public variables
 
+
+
+    #endregion
+
+    #region Private variables
+    [SerializeField] private int maxJumps;
+    [SerializeField] private float jumpingPower;
+    [SerializeField] private float speed;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+
+    private float horizontal;
+    private bool isFacingRight = true;
+    private int jumps;
+    #endregion
+
     void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        #region Movement mechanic
+
+        #region Movement
+
         horizontal = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
         #endregion
+
+    }
+
+    void Update()
+    {
+
+        #region Jump mechanics
+
+        if (Input.GetButtonDown("Jump") && jumps > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            jumps--;
+        }
 
         #region Reset jump
 
@@ -39,25 +70,6 @@ public class Player : MonoBehaviour
 
         #endregion
 
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        #region Jump mechanic
-
-        if (Input.GetButtonDown("Jump") && jumps > 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            
-        }
-
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-            jumps--;
-        }
         #endregion
 
         Flip();
